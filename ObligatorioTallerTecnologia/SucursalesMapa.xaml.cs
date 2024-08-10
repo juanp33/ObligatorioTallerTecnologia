3baee138862922cb8041981c98619b407bd6df49
Antonio-Llado <146167869+Antonio-Llado@users.noreply.github.com>
2024-08-09 21:39:43 -0300

    cambio mapa y 2 sucursales xd

--- ObligatorioTallerTecnologia/SucursalesMapa.xaml.cs
+++ ObligatorioTallerTecnologia/SucursalesMapa.xaml.cs
@@ -1,11 +1,38 @@
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
 namespace ObligatorioTallerTecnologia;
 
 public partial class SucursalesMapa : ContentPage
 {
-	public SucursalesMapa()
-	{
-		InitializeComponent();
+    public SucursalesMapa()
+    {
+        InitializeComponent();
 
 
-	}
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var geolocation = new GeolocationRequest(GeolocationAccuracy.High, TimeSpan.FromSeconds(10));
+        var location = await Geolocation.GetLocationAsync(geolocation);
+        map.MoveToRegion(MapSpan.FromCenterAndRadius(location, Distance.FromMiles(2)));
+
+        var pin = new Pin
+        {
+            Label = "Sucursal Punta",
+            Address = " Punta Shopping",
+            Type = PinType.Place,
+            Location = new Location(-34.941027, -54.934873)
+        };
+        var pin1 = new Pin
+        {
+            Label = "Sucursal Atlantico",
+            Address = "Atlantico Shopping",
+            Type = PinType.Place,
+            Location = new Location(-34.916300, -54.961057)
+        };
+        map.Pins.Add(pin);
+        map.Pins.Add(pin1);
+    }
 }
